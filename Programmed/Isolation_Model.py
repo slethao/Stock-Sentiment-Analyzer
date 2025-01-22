378892e6095820f1e4520d94f6cf79d382cc5bc7
Somer Le <thaosle4@gmail.com>
2025-01-22 15:32:29 -0500

    setup for the isolation forest class

---
+++ Programmed/Isolation_Model.py
@@ -0,0 +1,20 @@
+from sklearn.ensemble import IsolationForest
+
+class IsolationModel:
+    def __init__(self):
+        self._file_path = "Programmed/NVIDIA_STOCK_04.csv"
+        self._iso_forest = ""
+        self._path_length = ""
+        self._anomly_scores = ""
+    
+    def build_model(self):
+        pass
+    
+    def training_model(self):
+        pass
+
+    def decision_made(self): # prediction
+        pass
+
+    def result_found(self):
+        pass
