f273fdf6f679618fc0d18c8fd71bc7ee31ee1cd9
Somer Le <thaosle4@gmail.com>
2025-01-22 15:46:03 -0500

    creating the setup of of how the class will look like

---
+++ Programmed/Tensor_Model.py
@@ -0,0 +1,13 @@
+import tensorflow
+
+class TensorModel:
+    def __init__(self):
+        self._data = ""
+        self._session = ""
+        self._learning_rate = ""
+
+    def builing_model():
+        pass
+
+    def compile_model():
+        pass
