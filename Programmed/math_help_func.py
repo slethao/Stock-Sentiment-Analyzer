1c5df7d1724ade425be87dc85d0a51d6b76e6cb2
Somer Le <thaosle4@gmail.com>
2025-01-22 11:17:40 -0500

    modify the lower and upper bound to accomadate the swered values

--- Programmed/math_help_func.py
+++ Programmed/math_help_func.py
@@ -4,21 +4,25 @@ def median_of_dataset(data):
     else:
         return len(data)/2
     
-def low_inter_quart_range(quarter_one, quarter_three):
+def low_inter_quart_range(quarter_one, quarter_three, min_value):
     iqr = quarter_three - quarter_one
     low_bound = quarter_one - 1.5*iqr
+    if low_bound < min_value:
+        low_bound = min_value
     return low_bound
 
-def high_inter_quart_range(quarter_one, quarter_three):
+def high_inter_quart_range(quarter_one, quarter_three, max_value):
     iqr = quarter_three - quarter_one
     high_bound = quarter_three + 1.5*iqr
+    if high_bound > max_value:
+        high_bound = max_value()
     return high_bound
 
 def in_ascending_order(data):
     for index in range(len(data)-1):
         for index_two in range(len(data)-1-index):
             if data[index_two] > data[index_two +1]:
-                temp = data[index_two]
+                _ = data[index_two]
                 data[index_two] = data[index_two + 1]
-                data[index_two + 1] = temp
+                data[index_two + 1] = _
     return data
