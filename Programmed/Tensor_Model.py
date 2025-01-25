3087b7b52f6646b32a10ab581e85b8bad920ad4d
thaosle <thaosle4@gmail.com>
2025-01-25 17:55:43 -0500

    The tensor flow model works, still need to turn it into a class

--- Programmed/Tensor_Model.py
+++ Programmed/Tensor_Model.py
@@ -1,34 +1,41 @@
 import tensorflow
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-import pandas
-import numpy
-import cleaning_data as clean
+from tensorflow.keras.layers import Dense, Dropout
+import Isolation_Model as iso
+from sklearn.preprocessing import StandardScaler
 
-data = pandas.read_csv("Programmed/NVIDIA_STOCK_04.csv")
-features = data.copy()
-labels = features.pop('Adj Close')
-features = numpy.array(features)
-noramalize = layers.Normalization()
-noramalize.adapt(features)
+isolation_model = iso.IsolationModel("Adj Close")
+anomaly_scores = isolation_model.anomaly_results()
 
-norm_model = tensorflow.keras.Sequential([
-                                            noramalize,
-                                            Dense(128, activation='relu'),
-                                            Dropout(0.2),
-                                            Dense(64, activation='relu'),
-                                            Dropout(0.2),
-                                            Dense(32, activation='relu'),
-                                            Dropout(0.2),
-                                            Dense(1)
+#ensure the dates are on there
+data_used = isolation_model.get_data_used()
+date_column = data_used['Date']
+features = data_used.drop(columns=['Date', isolation_model.get_x_value().columns[0]]).values
+
+scaler = StandardScaler()
+features = scaler.fit_transform(features)
+
+data_set = tensorflow.data.Dataset.from_tensor_slices((features, anomaly_scores, date_column))
+data_set = data_set.shuffle(buffer_size = 1024).batch(32)
+
+norm_model = tensorflow.keras.Sequential([ 
+                                            tensorflow.keras.Input(shape=(features.shape[1],)),
+                                            Dense(128, activation='relu', kernel_regularizer=tensorflow.keras.regularizers.l2(0.01)),
+                                            Dropout(0.5),
+                                            Dense(64, activation='relu', kernel_regularizer=tensorflow.keras.regularizers.l2(0.01)),
+                                            Dropout(0.5),
+                                            Dense(32, activation='relu', kernel_regularizer=tensorflow.keras.regularizers.l2(0.01)),
+                                            Dropout(0.5),
+                                            Dense(1) 
 ])
 
 norm_model.compile(loss = tensorflow.keras.losses.MeanSquaredError(),
-                   optimizer = tensorflow.keras.optimizers.Adam())
-norm_model.fit(features, labels, epochs = 30, batch_size = 40) 
-#NOTE reduce number of loss (lose is around 4%)
-#NOTE make sure its not overfit or under fit
+                   optimizer = tensorflow.keras.optimizers.Adam(learning_rate = 0.001)) # learning rate is good
+
+result = norm_model.fit(features, isolation_model.get_x_value().values, epochs = 250, batch_size = 32,
+               callbacks = [tensorflow.keras.callbacks.EarlyStopping(monitor= 'val_loss', patience=10, restore_best_weights=True)],
+               validation_split=0.424
+              ) 
+
 #NOTE create a personal training method
 #NOTE recreate the class
 
@@ -82,20 +89,5 @@ norm_model.fit(features, labels, epochs = 30, batch_size = 40)
 #         pass
 
 # def main():
-#     file_path = "Programmed/NVIDIA_STOCK_04.csv"
-#     data_set = pandas.read_csv(file_path)
-#     all_features = data_set.copy()
-#     test_feature = all_features.pop("Adj Close")
-#     all_features = numpy.array(all_features)
-#     x_value = all_features  # x_train (all columns)
-#     y_value = test_feature.values  # y_train (the last column)
-#     learning_rate = 0.001
-#     batch = 25
-#     ten = TensorModel(file_path, x_value, y_value, batch, learning_rate) # through in values (first)
-#     ten.build_model().fit(x_value, y_value, epochs=5) # build
-#     glossary = clean.main()
-#     ten.train_model(numpy.array(glossary.keys()), glossary['Adj Close']) # train (parameter) 
-#     print("finsihed!!")
-#     # conslusion
 
 # main()
