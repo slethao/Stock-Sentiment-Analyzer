0be5282f81482c428e828113122b1cb1d027bfd1
thaosle <thaosle4@gmail.com>
2025-01-27 18:51:58 -0500

    added three methods to calculate the standard deviation for price range, wide threshold, and narrow threshold

--- Programmed/math_help_func.py
+++ Programmed/math_help_func.py
@@ -1,3 +1,5 @@
+import pandas
+
 def median_of_dataset(data):
     if len(data) % 2 != 0:
         return int(len(data)/2)
@@ -26,3 +28,30 @@ def in_ascending_order(data):
                 data[index_two] = data[index_two + 1]
                 data[index_two + 1] = _
     return data
+
+def avg_daily_range(filepath):
+    data = pandas.read_csv(filepath)
+    record_num = len(data.values)
+    sum = 0.0
+    for price in data['Price Range']:
+        sum += float(price)
+    return sum/record_num
+
+def standard_dev(avg_range, all_range):
+    """
+    σ = ( Σ(x - μ)² / N )^(1/2)
+    """
+    sum = 0
+    total = len(all_range)
+    for a_range in all_range:
+        sum += (a_range - avg_range)**2
+    std = (sum/total)**(1/2)
+    return std
+
+def wide_threshold(stand_dev):
+    threshold = stand_dev*1.5
+    return threshold
+
+def narrow_threshold(stand_dev):
+    threshold = stand_dev*1.5*-1
+    return threshold
