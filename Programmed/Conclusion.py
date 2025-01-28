f4bfa8a67ef6479e1cf3b2060326eabcc901dc5b
thaosle <thaosle4@gmail.com>
2025-01-27 20:11:58 -0500

    finding the price change by evaluating the threshold value and comaping the daily range

--- Programmed/Conclusion.py
+++ Programmed/Conclusion.py
@@ -2,22 +2,6 @@ import math_help_func as math
 import pandas
 
 class Conclusion:
-    """
-    Goal of this class: Find the daily returns and the price change
-    Daily Return = (Today's Closing Price - Yesterday's Closing Price) / Yesterday's Closing 
-    Today's Price = Close (regular trading session)
-    Yesterday's Price = Adj Close (stock splits and dividends and help with long term analysis (price change behaviro))
-    Example:
-        yesterday price  = 140
-        today price = 200
-
-        (200 - 140)/100 = 0.6 (for the ratio) = 60 % (for the percentage *multipy by 100*)
-    
-    Price Change:    
-    High and Low is the price at whcih the stokc is traded
-    price movement used in volume
-    """
-
     def __init__(self, filepath):
         self._csv_file = pandas.read_csv(filepath)
         self._today_price = self._csv_file["Close"].values
@@ -27,6 +11,9 @@ class Conclusion:
 
     def calculate_daily_return(self): # daily return
         """
+        Daily Return = (Today's Closing Price - Yesterday's Closing Price) / Yesterday's Closing 
+        Today's Price = Close (regular trading session)
+        Yesterday's Price = Adj Close (stock splits and dividends and help with long term analysis (price change behaviro))
         Example:
         yesterday price  = 140
         today price = 200
@@ -37,31 +24,37 @@ class Conclusion:
         daily_return = (self._today_price - self._yesterday_price) / self._yesterday_price
         return daily_return
 
-    def price_move(self): # for stock volume
+    def price_change(self): # for stock volume
         """
         High vol = strong buying or selling pressure
         Low vol = thin liquidity and increased risk
         1) put all ranges into a .csv and calculate the average daily range (done)
         2) calculate standard deviation (done)
-        3) set the threshold (wide or narrow range) (pending)
-        4) current day's range for th
+        3) set the threshold (wide or narrow range) (done)
+        4) current day's range for th (pending maybe put into a pie chart or something to show on tensor board)
         """
         low_stock_counter = 0
         all_ranges = []
+        file_path = "Programmed/ALL_PRICE_RANGES.csv"
         for stock in self._high_stock:
             all_ranges.append(stock - self._low_stock[low_stock_counter])
             low_stock_counter += 1
         price_range_data_frame = pandas.DataFrame({"Price Range": all_ranges})
-        price_range_data_frame.to_csv("Programmed/ALL_PRICE_RANGES.csv", index = False)
-        avg = math.avg_daily_range("Programmed/ALL_PRICE_RANGES.csv")
+        price_range_data_frame.to_csv(file_path, index = False)
+        avg = math.avg_daily_range(file_path)
         std = math.standard_dev(avg, all_ranges)
-        print(f"std {std}")
+        #print(f"std {std}")
         # print(f"Avg price range: {avg}")
+        for a_range in all_ranges:
+            if math.wide_threshold(std, avg) < a_range: # wide compared to the average price range
+                print("large than usual flucation")
+                print(f"wide: threshold {math.wide_threshold(std, avg)} and the samle ({a_range})")
+            if math.narrow_threshold(std, avg) > a_range: # narrow compared to the average price range
+                print(f"narrow threshold {math.narrow_threshold(std,avg)} and the samle ({a_range})")
+                print("smaller than usual flucation")
 
-        # if : # wide compared to the average price range
-        #     pass
-        # else: # narrow compared to the average price range
-        #     pass
-
-    def price_behave(self): # for price change
+    def price_move(self): # for price change
+        """
+        price movement used in volume
+        """
         pass
